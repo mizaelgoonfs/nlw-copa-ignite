7f61afd4d03d7e53eaa3a72918d01c04790c029b
mizaelgoonfs <miza14pg@gotmail.com>
2023-02-02 11:07:07 -0300

    create mobile Google Authentication

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -8,8 +8,6 @@ import { Button } from "../components/Button";
 export function SignIn(){
     const { signIn, user } = useAuth();
 
-    console.log('DADOS DO USUÁRIO =>', user);
-
     return(
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
